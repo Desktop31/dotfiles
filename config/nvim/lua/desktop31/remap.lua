2513b70d3ab796341eadb742da821a91ab3ebbfe
Desktop31 <KrejciT1@gmail.com>
2023-02-15 01:39:05 +0100

    added nvim keybind

--- config/nvim/lua/desktop31/remap.lua
+++ config/nvim/lua/desktop31/remap.lua
@@ -39,6 +39,7 @@ function _G.set_terminal_keymaps()
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('n', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
 end
